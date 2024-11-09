23044f29f81b4a63baf09ee8fb4d4fa7fc4d7d70
jamie-stephenson <jamie.t.stephenson@gmail.com>
2024-11-09 19:32:17 +0000

    Parallel encode
    `encode_dataset` now uses crossbeam-channel to send and encoding jobs
    to worker threads and lazily receives tokens on the fly into an iterator
    for writing to numpy shards.

--- src/commands/encode/mod.rs
+++ src/commands/encode/mod.rs
@@ -5,11 +5,19 @@ use datastructures::{Merge,Token};
 use save::save_tokens;
 use std::collections::{BinaryHeap,HashMap};
 use std::path::Path;
+use std::thread;
 
+use crossbeam_channel::unbounded;
 use pyo3::prelude::*;
 use pyo3::types::{PyIterator, PyString};
 use pyo3::exceptions::PyException;
 
+
+/// Encode a string to tokens
+#[pyfunction]
+pub fn encode(s: String, merge_map: HashMap<(u32,u32),u32>) -> Vec<u32> {
+    encode_string(s, &merge_map)
+}
 
 /// Encode a dataset and save as numpy shards. 
 #[pyfunction]
@@ -20,33 +28,74 @@ pub fn encode_dataset(
     shard_size: usize,
     rank: i32
 ) -> PyResult<()> {
-
+    
     let mut merge_map: HashMap<(u32,u32), u32> = HashMap::new();
 
     for (pair,token) in merges {
         merge_map.insert(pair, token);
     }
 
-    let tokens = generator
+    // Convert python generator into iterator that yields Strings
+    let strings = generator
         .into_iter()    
         .map(|s| {
             // TODO: Handle Result better, don't just unwrap
-            let string = s.unwrap()
-                .downcast::<PyString>().unwrap()
-                .to_string();
-            encode_string(string, &merge_map)
+            s
+                .unwrap()
+                .downcast::<PyString>()
+                .unwrap()
+                .to_string()
         });
-    
+
+    // Lazy parallel iterator that yields results on the fly
+    let tokens = get_tokens_iter(strings, merge_map);
+
+    // Wrtite to file as and when results are yielded
     save_tokens(tokens, Path::new(path), shard_size, rank)
         .map_err(|e| PyException::new_err(e.to_string()))
+    }
+    
+    
+fn get_tokens_iter<I>(string_iter: I, merge_map: HashMap<(u32,u32), u32>) -> impl Iterator<Item = Vec<u32>>
+where
+    I: IntoIterator<Item = String>,
+{
+    let n_threads = num_cpus::get();
+    let (work_sender, work_receiver) = unbounded::<String>();
+    let (result_sender, result_receiver) = unbounded::<Vec<u32>>();
+    
+    // Spawn worker threads
+    for _ in 0..n_threads {
+        let work_receiver = work_receiver.clone();
+        let result_sender = result_sender.clone();
+        let merge_map = merge_map.clone();
+    
+        thread::spawn(move || {
+            // Each worker thread processes items as they become available
+            for item in work_receiver.iter() {
+                let result = encode_string(item,&merge_map);
+                result_sender.send(result).unwrap();
+            }
+        });
+    }
+    
+    // Drop the extra sender to close the channel when done
+    drop(result_sender);
+    
+    // Spawn a thread to feed the iterator into the work channel
+    for s in string_iter {
+        work_sender.send(s).unwrap();
+    }
+
+    // Close the work channel to signal completion
+    drop(work_sender);
+    
+    // Return an iterator over the received results
+    result_receiver.into_iter()
 }
 
-/// Encode a string to integer tokens
-#[pyfunction]
-pub fn encode(s: String, merge_map: HashMap<(u32,u32),u32>) -> Vec<u32> {
-    encode_string(s, &merge_map)
-}
 
+/// Perform the actual BPE algorithm on the given string with the given `merge_map`.
 fn encode_string(s: String, merge_map: &HashMap<(u32,u32),u32>) -> Vec<u32> {
 
     let utf8 = s.as_bytes().to_vec();
@@ -114,10 +163,10 @@ fn encode_string(s: String, merge_map: &HashMap<(u32,u32),u32>) -> Vec<u32> {
         tokens[right_idx].width = 0; // right token is "swallowed"
         
         let left_token = tokens[merge.idx];
-
+        
         // Handle results of merge looking forward
         if let Some(next_idx) = left_token.next {
-
+            
             tokens[next_idx].prev = Some(merge.idx);
 
             let new_pair = (left_token.val,tokens[next_idx].val);
@@ -136,7 +185,7 @@ fn encode_string(s: String, merge_map: &HashMap<(u32,u32),u32>) -> Vec<u32> {
         }
     };
     tokens
-        .into_iter()
+    .into_iter()
         .filter_map(|token| (token.width != 0).then(|| token.val))
         .collect()
 }
